1adbbba5ca0c0f808513a941dd0acfe0a7d6aa41
Muhammed Ekinci <siraceddinekinci@gmail.com>
2021-01-02 02:54:59 -0500

    added seedDB.js file

---
+++ scripts/seedDB.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const User = require("../models/Users");
+
+mongoose.connect(process.env.MONGOBD_URI || 'mongodb+srv://MuhammedEkinci:*Tbn58kpm@cluster0.e9fkz.mongodb.net/semerkand_activity_log?retryWrites=true&w=majority' ,
+    {
+        useNewUrlParser: true
+    }
+)
+
+users = [
+    {
+        userID: "us-01-01",
+        password: "123456",
+        branch: "New Jersey",
+        name: "blah",
+        email: "blah@email.com",
+        phone: "1112223333"
+    }
+]
+
+User.collection 
+ .insertMany(user)
+ .then((userlog) => {
+     console.log(userlog);
+ })
+ .catch(({message}) => {
+    console.log(message)
+});
