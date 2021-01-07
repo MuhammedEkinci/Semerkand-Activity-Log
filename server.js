6277467835a13b86b8c02affa53aeb845b0dbc29
Muhammed Ekinci <siraceddinekinci@gmail.com>
2021-01-07 00:29:22 -0500

    working server

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 //dependencies needed for server side
+const compression = require("compression");
 const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const User = require("./models/Users");
+const Activity = require("./models/Activity-Log");
 
 
 const app = express();
 
 // Setting up port and requiring models for syncing
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+
+function shouldCompress (req, res) {
+    if (req.headers['x-no-compression']) {
+      // don't compress responses with this request header
+      return false
+    }
+   
+    // fallback to standard filter function
+    return compression.filter(req, res)
+}
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -36,13 +48,52 @@ app.listen(PORT, function() {
 //api-routes
 
 // post users into database
-app.post("api/users", (req, res) => {
+app.post("/api/users", (req, res) => {
     console.log("route hit!!");
     User.create(req.body).then((error, data) => {
         if(error) {
             res.send(error)
         } else {
             console.log(data)
+            res.json(data);
+        }
+    });
+});
+
+//get all users for admin to see and manage
+app.get("/api/users", (req, res) => {
+    console.log("route hit!!");
+    User.find({}, (error, data) => {
+        if(error) {
+            console.log(error)
+        }
+        else {
+            res.json(data);
+        }
+    });
+});
+
+//route to post activities to database
+app.post("/api/activities", (req, res) => {
+    console.log("route hit!!");
+    Activity.create(req.body).then((error, data) => {
+        if(error) {
+            res.send(error)
+        } else {
+            console.log(data)
+            res.json(data);
+        }
+    });
+});
+
+// route to get activities from database to render on webpage
+app.get("/api/activities", (req, res) => {
+    console.log("route hit!!");
+    Activity.find({}, (error, data) => {
+        if(error) {
+            console.log(error)
+        }
+        else {
             res.json(data);
         }
     });
