3df2696bdb993f8376e33b1fa3de025c41c4c08e
Muhammed Ekinci <siraceddinekinci@gmail.com>
2021-01-23 01:24:47 -0500

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
 import React from  "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Admin from './pages/Admin';
+import Login from './pages/Login';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/admin" component={Admin} />
+      </Router>
     </div>
   );
 }
--- models/Users.js
+++ models/Users.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+// Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
+var bcrypt = require("bcryptjs");
+
 const userSchema = new Schema({
     userID: {
         type: String,
