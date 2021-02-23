36676cbf795e4f7571ea364384513bc7f41f4676
Muhammed Ekinci <siraceddinekinci@gmail.com>
2021-02-22 23:57:10 -0500

    added routes for user signup

--- router/userRouter.js
+++ router/userRouter.js
@@ -9,3 +9,42 @@ const validateLoginInput = require("../validation/login");
 
 // Load User model
 const User = require("../models/Users");
+
+
+router.post("/signup", (req, res) => {
+
+    // Form validation
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    User.findOne({ email: req.body.email }).then(user => {
+        if(user) {
+            return res.status(400).json({ email: "Email already exists" });
+        } else {
+            const newUser = new User({
+                userID: req.body.userID,
+                email: req.body.email,
+                password: req.body.password,
+                branch: req.body.branch,
+                phone: req.body.phone
+            });
+
+            // Hash password before saving in database
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if(err) throw err;
+                    newUser.password = hash;
+                    newUser
+                        .save()
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err));
+                })
+            })
+        }
+    })
+
+});
