945ff936c7fe1e0d22a17653be0b9c7063d223fc
Muhammed Ekinci <siraceddinekinci@gmail.com>
2021-02-28 01:14:55 -0500

    added backend redux code

--- client/src/store.js
+++ client/src/store.js
@@ -1,12 +1,14 @@
 // reates a Redux store that holds the complete state tree of your app.
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
 const initialState = {};
+
 const middleware = [thunk];
 
 const store = createStore(
-  () => [],
+  rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
