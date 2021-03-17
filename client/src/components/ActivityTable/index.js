fc13c400923609cb5dd41a7b066025807ff7601c
Muhammed Ekinci <siraceddinekinci@gmail.com>
2021-03-17 02:09:30 -0400

    added filtering functionality to activity table

--- client/src/components/ActivityTable/index.js
+++ client/src/components/ActivityTable/index.js
@@ -1,99 +1,61 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import API from "../../utils/API";
-import {Table, Form, Col } from "react-bootstrap";
+import Datatable from "../Datatable";
+import {Table, Row, Col} from "react-bootstrap";
+import "./style.css";
 
-class ActivityTable extends Component {
+function ActivityTable() {
 
-    state = {
-        activitieList: [],
-        activitieListFiltered: [],
-        sorted: "ascend"
+    const [activities, setActivities] = useState([]);
+    const [q, setQ] = useState("");// this state will hold the filtering options
+    const [searchColumns, setSearchColumns] = useState(["branch", "country"]);
+
+    useEffect(() => {
+        getActivitiesToPost();
+    }, []);
+
+    // Get all the activities from database to display
+    const getActivitiesToPost = () => {
+        API.getActivities()
+            .then(({data}) => setActivities(data))
+            .catch((err) => console.log(err));
     };
 
-    // Call API to retrieve activities from the database
-    retrieveActivities = async () => {
-        try {
-            const res = await API.getActivities();
-            const activities = res.data.map(act => ({
-                branch: act.branch,
-                country: act.country,
-                activityType: act.activityType,
-                date: act.date,
-                time: act.time,
-                subject: act.subject,
-                speaker: act.speaker,
-                gender: act.gender,
-                ageGroup: act.ageGroup,
-                numParticipants: act.numParticipants,
-                hatmeType: act.hatmeType
-            }));
-            console.log(activities);
-            this.setState({activitieList: activities, activitieListFiltered: activities});
-
-        } catch(err) {
-            console.log(err);
-        }
-    };
-
-    //call retrieveActivities
-    componentDidMount() {
-        this.retrieveActivities();
-    }
-
-    render(){
-        return (
-            <>
-                <div id="table-filter-section">
-                    <Form>
-                        <Form.Row>
-                            <Col xs={12} md={4}>
-                                <Form.Control placeholder="First name" />   
-                            </Col>
-                        </Form.Row>
-                    </Form>
-                </div>
-                <h1>This is where Activities will render</h1>
-                <div>
-                    <Table className="table table-lg" striped bordered responsive="sm">
-                        <tr>
-                            <thead>
-                                <th>Branch</th>
-                                <th>Country</th>
-                                <th>Activity</th>
-                                <th>Participants</th>
-                                <th>Subject</th>
-                                <th>Speaker</th>
-                                <th>Gender</th>
-                                <th>AgeGroup</th>
-                                <th>Hatme type</th>
-                                <th>Date</th>
-                                <th>Time</th>
-                            </thead>
-                            <tbody>
-                                {this.state.activitieListFiltered.map(actInfo => {
-                                    return(
-                                        <tr key={actInfo.id}>
-                                            <td>{actInfo.branch}</td>
-                                            <td>{actInfo.country}</td>
-                                            <td>{actInfo.activityType}</td>
-                                            <td>{actInfo.numParticipants}</td>
-                                            <td>{actInfo.subject}</td>
-                                            <td>{actInfo.speaker}</td>
-                                            <td>{actInfo.gender}</td>
-                                            <td>{actInfo.ageGroup}</td>
-                                            <td>{actInfo.hatmeType}</td>
-                                            <td>{actInfo.date}</td>
-                                            <td>{actInfo.time}</td>
-                                        </tr>
-                                    );
-                                })}
-                            </tbody>
-                        </tr>
-                    </Table>
-                </div>
-            </>
+    function searchActivity(rows){
+        return rows.filter((row) => 
+            searchColumns.some(
+                (column) => 
+                    row[column].toString().toLowerCase().indexOf(q.toLocaleLowerCase()) > -1)
         );
     }
+
+    const columns = activities[0] && Object.keys(activities[0]);
+    console.log(columns);
+
+    return (
+        <>
+        {/* Table filter options*/}
+        <div className="page-section" id="filter-table">
+                <input id="activity-search" type="text" value={q} onChange={(e) => setQ(e.target.value)} placeholder="search activity"></input>
+                {
+                    columns && columns.map(column => <label>
+                        <input id="filter-checkbox" type="checkbox" checked={searchColumns.includes(column)}
+                            onChange={(e) => {
+                                const checked = searchColumns.includes(column)
+                                setSearchColumns(prev => checked
+                                    ? prev.filter(sc => sc !== column) : [...prev, column]);
+                            }}
+                        />
+                        {column}
+                    </label>)
+                }
+        </div>
+        {/* Show Activity table*/}
+        <div className="page-section" id="activity-table">
+            <Datatable data={searchActivity(activities)}/>     
+        </div>
+        </>
+    );
 }
 
 export default ActivityTable;
